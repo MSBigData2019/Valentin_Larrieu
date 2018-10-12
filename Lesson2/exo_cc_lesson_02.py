92aec24ff3886c866596da5d41e1969b942278dc
ValentinLarrieu22 <valentin.larrieu@telecom-paristech.fr>
2018-10-11 23:15:58 -0400

    first version scrapper on darty

---
+++ Lesson2/exo_cc_lesson_02.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import re
+
+website_prefix = "https://www.darty.com"
+# List of entries
+scrap_list = {
+    'Dell': "https://www.darty.com/nav/recherche/dell.html",
+    'Acer': "https://www.darty.com/nav/recherche/acer.html",
+}
+
+# List collecting the results
+results_dic_list = []
+
+
+
+
+def _clear_query(query):
+    new_query = re.sub(r'[^\w\s]', '', query.lower()).replace(" ",
+                                                              "+")  # we filter the words from upercase and punctuation and replace space by what we need
+    return new_query
+
+
+def _handle_request_result_and_build_soup(request_result):
+    if request_result.status_code == 200:
+        html_doc = request_result.text
+        soup = BeautifulSoup(html_doc, "html.parser")
+        return soup
+
+
+def _convert_value_to_float(string):
+    if "--" in string:
+        return 0
+    value = float(
+        re.findall("[+-]?\d+\.\d+", string.strip().replace(",", ""))[0])  # On ne récupère que les chiffres et le signe
+    return value
+
+
+def print_results(dict):
+    print("""
+    Nom {name} 
+    Pourcentage moyen {perc}
+    """.format(name=dict["name"],perc=dict["average_pourc"]))
+
+
+def main():
+    for key, value in scrap_list.items():
+        # Initialization
+        data = {'name': key}
+        page = requests.get(value)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        reduc_table = soup.find_all(class_='darty_prix_barre_remise darty_small separator_top')
+        print(reduc_table)
+
+        if (reduc_table is not None):
+            print(reduc_table)
+
+            sum = 0
+            for pourc in reduc_table:
+                sum = sum + _convert_value_to_float(pourc)
+
+            if (len(reduc_table) != 0 ):
+                data["average_pourc"] = sum / len(reduc_table)
+            else:
+                data["average_pourc"] = 0
+
+        else:
+            data["average_pourc"] = 0
+
+
+        # specific_class = "test"
+        # all_links = list(map(lambda x: x.attrs['href'], soup.find_all("a", class_=specific_class))) # We get all the links
+
+        # results_dic_list.append(data)
+
+    # Print of the results
+    for dict in results_dic_list:
+        print_results(dict)
+    if results_dic_list[0] > results_dic_list[1]:
+        print("dell is the winner !")
+    else:
+        print("acer is the winner !")
+
+
+
+
